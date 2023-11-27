393497422fbdb0bd9ca91f2716df7458e8ae6621
kelompokptik <cyberlast173@gmail.com>
2023-11-27 11:16:38 +0700

    #7 Click Events

--- NetNinjaReactJs/src/Home.jsx
+++ NetNinjaReactJs/src/Home.jsx
@@ -1,7 +1,17 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log("Welcome to my WEB", e);
+    };
+
+    const handleClickAgain = (name, e) => {
+        console.log(`Hello my name is ${name}`, e);
+    };
+    
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e)=> handleClickAgain('Ubay',e.target)}>Click Again</button>
         </div>
      );
 }
