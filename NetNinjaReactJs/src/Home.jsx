b3382be731ccbc378b66f3b3828758271b11d504
kelompokptik <cyberlast173@gmail.com>
2023-11-27 12:38:22 +0700

    #8 Using State

--- NetNinjaReactJs/src/Home.jsx
+++ NetNinjaReactJs/src/Home.jsx
@@ -1,19 +1,21 @@
-const Home = () => {
-    const handleClick = (e) => {
-        console.log("Welcome to my WEB", e);
-    };
+import { useState } from "react";
 
-    const handleClickAgain = (name, e) => {
-        console.log(`Hello my name is ${name}`, e);
-    };
-    
-    return ( 
-        <div className="home">
-            <h2>Homepage</h2>
-            <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e)=> handleClickAgain('Ubay',e.target)}>Click Again</button>
-        </div>
-     );
-}
- 
+const Home = () => {
+  let [name, setName] = useState("Ubay");
+  let [age, setAge] = useState(18);
+
+  const handleClickAgain = () => {
+    setName("Dillah");
+    setAge(19);
+  };
+
+  return (
+    <div className="home">
+      <h2>Homepage</h2>
+      <p>My name is {name}, and im {age} years old</p>
+      <button onClick={handleClickAgain}>Click Again</button>
+    </div>
+  );
+};
+
 export default Home;
