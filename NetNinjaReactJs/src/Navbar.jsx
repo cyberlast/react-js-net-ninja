04c7d59c4c133317dc92ac6134a971c70cf21cd6
kelompokptik <cyberlast173@gmail.com>
2023-11-27 10:09:12 +0700

    #5

---
+++ NetNinjaReactJs/src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>Kenzy</h1>
+            <div className="links">
+                <a href="/">Home</a>
+                <a href="/crate">New Block</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
